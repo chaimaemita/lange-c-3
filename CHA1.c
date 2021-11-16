4e5ca0cd0d0fdc7d290d4b4cfe97a0b845a33bfc
chaimae mita <chaimae.mita@gmail.com>
2021-11-16 17:00:06 +0100

    modofication

---
+++ CHA1.c
@@ -0,0 +1,9 @@
+algorithme table de multiplication
+variable nbr,i,mul:entier
+ Debut
+écrir('entrez un nombre')
+lire (nbr)
+pour i allont de 0 à 10 faire
+mul<- i*nbr
+écrire ('le table de multiplication est:',mul)
+ Fin 
